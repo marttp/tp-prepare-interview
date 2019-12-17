26b53d2daf9b2807253989cae9d634ebf5a98388
Thanaphoom.b <tp.babparn@gmail.com>
2019-12-17 13:48:45 +0700

    Commit 4: Add Shell sort

--- JavaDataStructure/Sort/src/com/training/Main.java
+++ JavaDataStructure/Sort/src/com/training/Main.java
@@ -12,7 +12,9 @@ public class Main {
         }
 
 //        bubbleSort(intArray);
-        selectionSort(intArray);
+//        selectionSort(intArray);
+//        insertionSort(intArray);
+        shellSort(intArray);
         System.out.println(Arrays.toString(intArray));
 
     }
@@ -26,7 +28,7 @@ public class Main {
         array[j] = tmp;
     }
 
-    // O(n^2) Quadratic
+    // O(n^2) Quadratic : Unstable
     private static void bubbleSort(int[] unsortedArray) {
         for (int i = unsortedArray.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
@@ -37,7 +39,7 @@ public class Main {
         }
     }
 
-    // O(n^2) Quadratic
+    // O(n^2) Quadratic : Unstable
     private static void selectionSort(int[] unsortedArray) {
         for (int i = 0; i < unsortedArray.length; i++) {
             int minIndexEachRound = i;
@@ -47,6 +49,35 @@ public class Main {
                 }
             }
             swap(unsortedArray, i, minIndexEachRound);
+        }
+    }
+
+    // O(n^2) Quadratic
+    private static void insertionSort(int[] unsortedArray) {
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < unsortedArray.length; firstUnsortedIndex++) {
+            int element = unsortedArray[firstUnsortedIndex];
+            int index;
+            for (index = firstUnsortedIndex; index > 0 && unsortedArray[index - 1] > element; index--) {
+                unsortedArray[index] = unsortedArray[index - 1];
+            }
+            unsortedArray[index] = element;
+        }
+    }
+
+    // O(n^2) Worst case but can perform much better : Unstable
+    private static void shellSort(int[] unsortedArray) {
+        // Knuth Sequence : k
+        // Gap calculate : (3^k -1 ) / 2
+        for (int gap = unsortedArray.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < unsortedArray.length; i++) {
+                int element = unsortedArray[i];
+                int index = i;
+                while (index >= gap && unsortedArray[index - gap] > element) {
+                    unsortedArray[index] = unsortedArray[index - gap];
+                    index -= gap;
+                }
+                unsortedArray[index] = element;
+            }
         }
     }
 }
