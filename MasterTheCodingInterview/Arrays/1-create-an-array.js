f71981eb5f96e3f2e659270ccde0502f31247664
Thanaphoom.b <tp.babparn@gmail.com>
2019-12-22 04:59:24 +0700

    Add array section [JS]

---
+++ MasterTheCodingInterview/Arrays/1-create-an-array.js
@@ -0,0 +1,40 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.data;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  deleteAtIndex(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+
+  shiftItems(index) {
+    // Move all next value replace  the old ones
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    console.log(this.data[this.length - 1]);
+    // Then delete it. Previous value is on before index
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
