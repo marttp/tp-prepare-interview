5c370a934a90d758f92e564ec19612bf3f60e06f
Thanaphoom.b <tp.babparn@gmail.com>
2019-12-25 12:16:23 +0700

    Add RadixSort string [Java]

--- JavaDataStructure/Sort/src/com/training/sort/RadixSort.java
+++ JavaDataStructure/Sort/src/com/training/sort/RadixSort.java
@@ -11,6 +11,7 @@ public class RadixSort extends Sort {
     @Override
     public void sort() {
         radixSort(10, 4);
+//        radixSort(26, 5);
     }
 
     private void radixSort(int radix, int digit) {
@@ -18,12 +19,18 @@ public class RadixSort extends Sort {
         for (int i = 0; i < digit; i++) {
             radixSingleSort(i, radix);
         }
+//        for (int i = digit - 1; i >= 0; i--) {
+//            radixSingleSort(i, radix);
+//        }
     }
 
     private void radixSingleSort(int position, int radix) {
         int numItems = this.array.length;
         int[] countArray = new int[radix];
 
+//        for (String value: this.array) {
+//            countArray[getIndex(position, value)]++;
+//        }
         for (int value : this.array) {
             countArray[getDigit(position, value, radix)]++;
         }
@@ -41,6 +48,7 @@ public class RadixSort extends Sort {
 //        System.out.println("countArray");
 //        System.out.println(Arrays.toString(countArray));
 
+//        String[] temp = new String[numItems];
         int[] temp = new int[numItems];
         for (int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
             int digit = getDigit(position, this.array[tempIndex], radix);
@@ -60,7 +68,13 @@ public class RadixSort extends Sort {
         }
     }
 
-    private int getDigit(int position, int value, int radix) {
+
+    private static int getDigit(int position, int value, int radix) {
         return value / (int) Math.pow(radix, position) % radix;
     }
+
+    public static int getIndex(int position, String value) {
+        return value.charAt(position) - 'a';
+    }
+
 }
