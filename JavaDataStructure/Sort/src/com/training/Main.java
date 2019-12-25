d28c3e02e8f0d9f3efb9ef1704d2ea0e952a1213
Thanaphoom.b <tp.babparn@gmail.com>
2019-12-25 10:48:12 +0700

    Add Radix sort [Java]

--- JavaDataStructure/Sort/src/com/training/Main.java
+++ JavaDataStructure/Sort/src/com/training/Main.java
@@ -10,7 +10,8 @@ public class Main {
         // write your code here
         int[] intArray = new int[10];
         for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = (int)(Math.random() * 10);
+//            intArray[i] = (int)(Math.random() * 10);
+            intArray[i] = (int)(Math.random() * 10000);
         }
         Sort sort;
 //        sort = new BubbleSort(intArray);
@@ -19,8 +20,16 @@ public class Main {
 //        sort = new ShellSort(intArray);
 //        sort = new MergeSort(intArray);
 //        sort = new QuickSort(intArray);
-        sort = new CountingSort(intArray);
+//        sort = new CountingSort(intArray);
+        sort = new RadixSort(intArray);
         sort.sort();
+
+//        Arrays.sort(intArray);
+        /**
+         * Parallel sort more efficient on massive of data
+         */
+//        Arrays.parallelSort(intArray);
+
         System.out.println(Arrays.toString(intArray));
 //        int factorialResult = Factorial.factorial(5);
 //        System.out.println(factorialResult);
--- JavaDataStructure/Sort/src/com/training/sort/RadixSort.java
+++ JavaDataStructure/Sort/src/com/training/sort/RadixSort.java
@@ -1,12 +1,66 @@
 package com.training.sort;
 
+import java.util.Arrays;
+
 public class RadixSort extends Sort {
 
-    public RadixSort() {
+    public RadixSort(int[] array) {
+        this.array = array;
     }
 
     @Override
     public void sort() {
+        radixSort(10, 4);
+    }
 
+    private void radixSort(int radix, int digit) {
+        // Iterate will loop over array. digit is up to maximum of possible value
+        for (int i = 0; i < digit; i++) {
+            radixSingleSort(i, radix);
+        }
+    }
+
+    private void radixSingleSort(int position, int radix) {
+        int numItems = this.array.length;
+        int[] countArray = new int[radix];
+
+        for (int value : this.array) {
+            countArray[getDigit(position, value, radix)]++;
+        }
+
+//        System.out.println("countArray");
+//        System.out.println(Arrays.toString(countArray));
+
+        // Count all of it
+        for (int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j - 1];
+        }
+
+//        System.out.println("array");
+//        System.out.println(Arrays.toString(this.array));
+//        System.out.println("countArray");
+//        System.out.println(Arrays.toString(countArray));
+
+        int[] temp = new int[numItems];
+        for (int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
+            int digit = getDigit(position, this.array[tempIndex], radix);
+            // Use count array to specific index of tmp value
+            int index = --countArray[digit];
+            temp[index] = this.array[tempIndex];
+        }
+
+//        System.out.println("countArray");
+//        System.out.println(Arrays.toString(countArray));
+//        System.out.println("temp");
+//        System.out.println(Arrays.toString(temp));
+//        System.out.println();
+
+        for (int tempIndex = 0; tempIndex < numItems; tempIndex++) {
+            this.array[tempIndex] = temp[tempIndex];
+        }
+    }
+
+    private int getDigit(int position, int value, int radix) {
+        return value / (int) Math.pow(radix, position) % radix;
     }
 }
