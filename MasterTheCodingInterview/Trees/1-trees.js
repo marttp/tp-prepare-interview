df4746bcaf3793e81ad08845bc752488ca7c3f55
Thanaphoom.b <tp.babparn@gmail.com>
2019-12-29 13:20:16 +0700

    JS: Refactor BST, Add remove function

--- MasterTheCodingInterview/Trees/1-trees.js
+++ MasterTheCodingInterview/Trees/1-trees.js
@@ -1,63 +1,4 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-
-  insert(value) {
-    //Code here
-    if (this.root === null || this.root === undefined) {
-      this.root = new Node(value);
-    } else {
-      let currentNode = this.root;
-      while (true) {
-        if (value < currentNode.value) {
-          if (!currentNode.left) {
-            currentNode.left = new Node(value);
-            return;
-          } else {
-            currentNode = currentNode.left;
-          }
-        } else {
-          if (!currentNode.right) {
-            currentNode.right = new Node(value);
-            return;
-          } else {
-            currentNode = currentNode.right;
-          }
-        }
-      }
-    }
-  }
-
-  lookup(value) {
-    if (this.root === null || this.root === undefined) {
-      return null;
-    }
-    let currentNode = this.root;
-    while (currentNode) {
-      if (currentNode.value === value) {
-        console.log(`found data`);
-        return currentNode;
-      } else if (value < currentNode.value) {
-        currentNode = currentNode.left;
-      } else if (value > currentNode.value) {
-        currentNode = currentNode.right;
-      }
-    }
-    console.log(`${value} not found in BST`);
-    return null;
-  }
-
-  // remove
-}
+const BinarySearchTree = require('./BinarySearchTree');
 
 const tree = new BinarySearchTree();
 tree.insert(9);
@@ -80,8 +21,8 @@ tree.insert(1);
 
 
 // ! Traverse by Me
-// traverse(tree.root, 'infix');
-console.log(JSON.stringify(tree.lookup(30)));
+traverse(tree.root, 'infix');
+// console.log(JSON.stringify(tree.lookup(30)));
 
 function traverse(node, traverseOptions = 'postfix') {
   if (node) {
