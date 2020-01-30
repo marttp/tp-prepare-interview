abc34a9c9c285a81a281478187b267e8510e861f
Thanaphoom.b <tp.babparn@gmail.com>
2020-01-30 10:53:38 +0700

    Java: Add HashTable [LinearProbe]

---
+++ JavaDataStructure/HashTable/src/linearprobe/SimpleHashtable.java
@@ -0,0 +1,109 @@
+package linearprobe;
+
+public class SimpleHashtable {
+
+    private StoredEmployee[] hashtable;
+
+    public SimpleHashtable() {
+        hashtable = new StoredEmployee[10];
+    }
+
+    // Reserved
+    private boolean occupied(int index) {
+        return hashtable[index] != null;
+    }
+
+    public void put(String key, Employee employee) {
+        int hashedKey = hashKey(key);
+        if (occupied(hashedKey)) {
+            int stopIndex = hashedKey;
+            if (hashedKey == hashtable.length - 1) {
+                hashedKey = 0;
+            } else {
+                hashedKey++;
+            }
+
+            // First condition is more important because if return true will find new empty slot
+            while (occupied(hashedKey) && hashedKey != stopIndex) {
+                hashedKey = (hashedKey + 1) % hashtable.length;
+            }
+        }
+
+        // After finish the while loop. We got the empty slot or second condition in while loop take false Then position already used.
+        if (occupied(hashedKey)) {
+            System.out.println("Sorry, there's already an employee at position " + hashedKey);
+        } else {
+            hashtable[hashedKey] = new StoredEmployee(key, employee);
+        }
+    }
+
+    public Employee get(String key) {
+        int hashedKey = findKey(key);
+        if (hashedKey == -1) {
+            return null;
+        }
+        return hashtable[hashedKey].employee;
+    }
+
+    public Employee remove(String key) {
+        int hashedKey = findKey(key);
+        if (hashedKey == -1) {
+            return null;
+        }
+
+        Employee employee = hashtable[hashedKey].employee;
+        hashtable[hashedKey] = null;
+
+        // Reinitial hash table
+        StoredEmployee[] oldHashtable = hashtable;
+        hashtable = new StoredEmployee[oldHashtable.length];
+        for (int i = 0; i < oldHashtable.length; i++) {
+            if (oldHashtable[i] != null) {
+                put(oldHashtable[i].key, oldHashtable[i].employee);
+            }
+        }
+
+        return employee;
+    }
+
+    private int hashKey(String key) {
+        return key.length() % hashtable.length;
+    }
+
+    private int findKey(String key) {
+        int hashedKey = hashKey(key);
+
+        // Case 1 : Key match index && key store
+        if (hashtable[hashedKey] != null &&
+                hashtable[hashedKey].key.equals(key)) {
+            return hashedKey;
+        }
+
+        int stopIndex = hashedKey;
+        if (hashedKey == hashtable.length - 1) {
+            hashedKey = 0;
+        } else {
+            hashedKey++;
+        }
+
+        while (hashedKey != stopIndex &&
+                hashtable[hashedKey] != null &&
+                !hashtable[hashedKey].key.equals(key)) {
+            // If it not match. Iterate +1 until it's find the match Key store
+            hashedKey = (hashedKey + 1) % hashtable.length;
+        }
+
+        return hashtable[hashedKey] != null && hashtable[hashedKey].key.equals(key) ? hashedKey : -1;
+    }
+
+    public void printHashtable() {
+        for (int i = 0; i < hashtable.length; i++) {
+            if (hashtable[i] == null) {
+                System.out.println("empty");
+            } else {
+                System.out.println("Position " + i + ": " + hashtable[i].employee);
+            }
+        }
+    }
+
+}
