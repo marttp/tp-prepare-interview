68aa2d5878425e80f31bcdea7920cfe505f0b1bf
Thanaphoom.b <tp.babparn@gmail.com>
2020-02-05 11:13:16 +0700

    Java: Add chaining & bucket sort & challenge example

---
+++ JavaDataStructure/HashTable/src/standard/Main.java
@@ -0,0 +1,27 @@
+package standard;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+        Map<String, Employee> hashMap = new HashMap<String, Employee>();
+        hashMap.put("Jones", janeJones);
+        hashMap.put("Doe", johnDoe);
+        hashMap.put("Smith", marySmith);
+
+        Employee employee = hashMap.putIfAbsent("Doe", mikeWilson);
+        System.out.println(employee);
+        System.out.println(hashMap.getOrDefault("someone", mikeWilson));
+        System.out.println(hashMap.remove("Jones"));
+        hashMap.forEach((k, v) -> System.out.println("Key = " + k + ", Employee = " + v));
+
+    }
+}
