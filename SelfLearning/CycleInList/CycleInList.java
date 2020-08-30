dbbb6d53451d789097ef09f9ffd523efce73a48a
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-08-30 17:53:10 +0700

    Change code to hare & tortoise

--- SelfLearning/CycleInList/CycleInList.java
+++ SelfLearning/CycleInList/CycleInList.java
@@ -5,22 +5,26 @@ public class CycleInList {
     private class Node {
         int data;
         Node next;
+
         public Node(int data) {
             this.data = data;
         }
     }
 
     boolean hasCycle(Node head) {
-        if (head == null) return false;
+        if (head == null || head.next == null)
+            return false;
+
         Node fast = head;
         Node slow = head;
 
-        while (fast != null && slow != null) {
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
             if (fast == slow) {
                 return true;
             }
-            fast = fast.next.next;
-            slow = fast.next;
         }
 
         return false;
