b4620a36155591e0594e93a5defee105e1c13436
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-08-30 18:48:07 +0700

    Add own linked list & Check BST

---
+++ SelfLearning/IsThisBST/IsThisBST.java
@@ -0,0 +1,47 @@
+public class IsThisBST {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null))
+            return true;
+        return checkBSTRecursive(root, null, null);
+        // return checkBSTIterative(root);
+    }
+
+    public boolean checkBSTRecursive(TreeNode node, Integer lower, Integer upper) {
+        if (node == null)
+            return true;
+        // Check current binary search
+        if (lower != null && node.val <= lower)
+            return false;
+        if (upper != null && node.val >= upper)
+            return false;
+
+        // Dive to check children both sides
+        if (!checkBSTRecursive(node.right, node.val, upper))
+            return false;
+        if (!checkBSTRecursive(node.left, lower, node.val))
+            return false;
+
+        return true;
+    }
+
+    public boolean checkBSTIterative(TreeNode node) {
+        Stack<TreeNode> stack = new Stack();
+        double inorder = -Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || node != null) {
+            // Move root next until leaf node and push to stack
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            // Then we got leaf node => pop to check
+            root = stack.pop();
+            // If it's lower than current Inorder value
+            if (root.val <= inorder)
+                return false;
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
